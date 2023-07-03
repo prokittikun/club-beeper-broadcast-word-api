ca6545b9ceca0bd9653e09a92948b1a723ba049a
prokittikun <pro.kittikun.2546@gmail.com>
2023-07-03 20:33:21 +0700

    Add: mongo url

--- src/app.module.ts
+++ src/app.module.ts
@@ -9,7 +9,7 @@ import { GameHostModule } from './game-host/game-host.module';
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb://localhost:27017/testDB?retryWrites=true&w=majority',
+      'mongodb+srv://kittikun:NWtwrnpjmN9y0gs5@cluster0.knw5i9i.mongodb.net/testDB?retryWrites=true&w=majority', //mongodb://localhost:27017/testDB?retryWrites=true&w=majority
   ),
     SocketModule,
     GameHostModule,
