c8d009d34b0deea3ef67a81c8b4b1eed00c98000
prokittikun <pro.kittikun.2546@gmail.com>
2023-07-03 20:34:16 +0700

    Connect mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,13 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { SocketModule } from './socket/socket.module';
 import { LogService } from './services/log.service';
 import { GameHostModule } from './game-host/game-host.module';
+import { config } from "dotenv";
+config();
 
 @Module({
   imports: [
     MongooseModule.forRoot(
-      'mongodb+srv://kittikun:NWtwrnpjmN9y0gs5@cluster0.knw5i9i.mongodb.net/testDB?retryWrites=true&w=majority', //mongodb://localhost:27017/testDB?retryWrites=true&w=majority
+      process.env.MONGO_URL, //mongodb://localhost:27017/testDB?retryWrites=true&w=majority
   ),
     SocketModule,
     GameHostModule,
